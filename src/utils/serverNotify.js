e3784f09329ca552d32185917a71289c97af4725
Li15838287467 <56282383+Li15838287467@users.noreply.github.com>
2022-01-27 18:28:20 +0800

    Update serverNotify.js

--- src/utils/serverNotify.js
+++ src/utils/serverNotify.js
@@ -6,33 +6,8 @@
 const sendMail = require("./sendMail");
 const config = require("./config.js");
 
-// function send({ pushKey, title, desp }) {
-//   return new Promise((resolve, reject) => {
-//     const option = {
-//       url: `https://sctapi.ftqq.com/${pushKey}.send`,
-//       method: "post",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       params: {
-//         title,
-//         desp,
-//       },
-//     };
-//     axios(option).then(
-//       res => {
-//         resolve();
-//         console.log("发送成功");
-//       },
-//       err => {
-//         reject(err);
-//       }
-//     );
-//   });
-// }
-
-// let msg = "掘金：\n";
 module.exports = function bot(message) {
+  console.log(config.TO);
   sendMail({
     form: "掘金",
     to: config.TO,
@@ -41,16 +16,4 @@ module.exports = function bot(message) {
   }).catch(error => {
     console.log(`发送失败 => ${error}`);
   });
-  // console.log("mes", message);
-  // if (config.SERVERID) {
-  //   msg += message + "\n";
-  //   send({
-  //     pushKey: config.SERVERID, // 企业
-  //     title: config.TEL + "定时任务", // title
-  //     to: config.TO, // 邮件发给谁
-  //     desp: msg, // desp
-  //   }).catch(error => {
-  //     console.log(`发送失败 => ${error}`);
-  //   });
-  // }
 };
